692aa7725e6ca5d880acd5b15001ececb35d2774
CapstoneVIP <drakebell09@hotmail.com>
2018-02-25 22:36:39 -0600

    Angular2 CRUD application with Karma/Jasmine Test Runner! Had to commit only the /src directory due to size limitations

---
+++ Angular/ngCRUD/src/app/app.component.ts
@@ -0,0 +1,60 @@
+import { Component, OnInit } from '@angular/core';
+import { TodoDataService } from './todo-data-service.service';
+import { Todo } from './todo';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+  providers: [TodoDataService]
+})
+export class AppComponent implements OnInit {
+
+  //Public property set to empty
+  todos: Todo[] = [];
+
+  constructor(
+    private todoDataService: TodoDataService
+  ) {
+  }
+
+  public ngOnInit() {
+    this.todoDataService
+      .getAllTodos()        //Get all 'todos' currently stored
+      .subscribe(           //Subscribe to Obserable returned
+        (todos) => {        //Values come in, overwrite inital empty array
+          this.todos = todos;
+        }
+      );
+  }
+
+  onAddTodo(todo) {
+    this.todoDataService
+      .addTodo(todo)
+      .subscribe(
+        (newTodo) => {
+          this.todos = this.todos.concat(newTodo);
+        }
+      );
+  }
+
+  onToggleTodoComplete(todo) {
+    this.todoDataService
+      .toggleTodoComplete(todo)
+      .subscribe(
+        (updatedTodo) => {
+          todo = updatedTodo;
+        }
+      );
+  }
+
+  onRemoveTodo(todo) {
+    this.todoDataService
+      .deleteTodoById(todo.id)
+      .subscribe(
+        (_) => {
+          this.todos = this.todos.filter((t) => t.id !== todo.id);
+        }
+      );
+  }
+}
